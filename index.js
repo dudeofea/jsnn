5ee6c78b3b8eeb5f73021d59fba05e062948e083
Denis Lachance <dudeofea@hotmail.com>
2015-10-11 16:20:04 -0600

    mostly fixed arrow drawing

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 $(window).load(function(){
 	var neurons = [];
+	var links = [];
 	var cur_offset = null;
+	var cur_elem = null;
 	var create_link = false;
 	//start drag
 	var neuron_mousedown = function(e){
@@ -8,21 +10,26 @@ $(window).load(function(){
 		if(create_link){
 			//create a link from last neuron to current
 			create_link = false;
-			create_link_func(cur_elem, target);
+			create_link_func(neurons[cur_elem-1].elem, target);
 		}
 		//set this as the current element
-		cur_elem = target;
+		cur_elem = parseInt(target.attr('id').replace('neuron-', ''));
 		$('neuron').removeClass('highlight');
-		cur_elem.addClass('highlight');
+		neurons[cur_elem-1].elem.addClass('highlight');
 		//update offset
-		var elem_o = cur_elem.offset();
+		var elem_o = neurons[cur_elem-1].elem.offset();
 		cur_offset = {x: elem_o.left - e.pageX, y: elem_o.top - e.pageY};
 	};
 	//during drag
 	$(window).on('mousemove', function(e){
 		if(cur_offset == null || cur_elem == null){ return; }
+		var id = cur_elem;
 		var elem_o = {left: cur_offset.x + e.pageX, top: cur_offset.y + e.pageY}
-		cur_elem.offset(elem_o);
+		neurons[id-1].elem.offset(elem_o);
+		//update outgoing arrow position
+		for (var i = 0; i < neurons[id-1].out.length; i++) {
+			update_arrow_func(neurons[id-1].out[i]);
+		}
 	//end drag
 	}).on('mouseup', function(){
 		cur_offset = null;
@@ -30,10 +37,10 @@ $(window).load(function(){
 	}).on('keypress', function(e){
 		if(e.which == 110){	//N key
 			//insert neuron, bind event
-			var id = neurons.push({out: []});
+			var id = neurons.push({out: [], elem: null});
 			var elem = $('<neuron id="neuron-'+id+'"><input type="text"/></neuron>');
 			$('body').append(elem);
-			elem.offset({top: "50%", left: "50%"});
+			neurons[id-1].elem = elem;
 		}else if(e.which == 109){	//M key
 			if(cur_elem == null){ return; }
 			create_link = true;
@@ -52,27 +59,44 @@ $(window).load(function(){
 	});
 	//create a link between two neurons
 	var create_link_func = function(na, nb){
+		//add link to array
+		var aid = parseInt(na.attr('id').replace('neuron-', ''));
+		var bid = parseInt(nb.attr('id').replace('neuron-', ''));
+		var id = links.push({elem: null, a: aid, b: bid});
+		neurons[aid-1].out.push(id);
+		//add an arrow between elements
+		var arrow = $('<arrow id="arrow-'+id+'"></arrow>');
+		$('body').append(arrow);
+		//add element to array
+		links[id-1].elem = arrow;
+		//update position
+		update_arrow_func(id);
+	};
+	var update_arrow_func = function(id){
+		//get elements
+		var arrow = links[id-1].elem;
+		var na = neurons[links[id-1].a-1].elem;
+		var nb = neurons[links[id-1].b-1].elem;
+		//get centers
 		var na_offset = na.offset();
 		var na_center = {left: na_offset.left + na.width() / 2, top: na_offset.top + na.height() / 2};
 		var nb_offset = nb.offset();
 		var nb_center = {left: nb_offset.left + nb.width() / 2, top: nb_offset.top + nb.height() / 2};
-		create_arrow_func(na_center, nb_center);
-	};
-	var create_arrow_func = function(pa, pb){
-		var arrow = $('<arrow></arrow>');
-		$('body').append(arrow);
+		var pa = na_center;
+		var pb = nb_center;
+		//update arrow position
 		var xdiff = pb.left - pa.left;
 		var ydiff = pb.top - pa.top;
 		var angle = Math.atan2(ydiff, xdiff);
 		var mag = Math.sqrt(xdiff*xdiff+ydiff*ydiff);
 		var a_offset = {
-			left: pa.left - (mag/2)*(1 - Math.cos(angle)),
-			top:  pa.top  + (mag/2)*(Math.sin(angle))
+			left: pa.left + xdiff/2 - Math.abs((mag/2)*Math.cos(angle)),
+			top:  pa.top  + ydiff/2 - Math.abs((mag/2)*Math.sin(angle))
 		}
+		console.log(angle, (mag/2)*Math.cos(angle));
 		arrow.offset(a_offset);
 		arrow.width(mag);
 		arrow.rotate(angle);
-		console.log(pa.left, pa.top, (mag/2)*(1 - Math.sin(angle)));
 	}
 });
 
